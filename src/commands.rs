ad2d235aef24a7892345589b3f8c52fe2052c9ab
PrivateRookie <996514515@qq.com>
2020-05-07 23:49:29 +0800

    feat: 修改命令行传参方式

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,25 @@
 use bytes::{BufMut, BytesMut};
 use structopt::StructOpt;
 
+#[derive(Debug, Clone)]
+pub enum ExistOP {
+    NX,
+    XX,
+}
+
+impl std::str::FromStr for ExistOP {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.to_ascii_lowercase() == String::from("nx") {
+            Ok(ExistOP::NX)
+        } else if s.to_ascii_lowercase() == String::from("xx") {
+            Ok(ExistOP::XX)
+        } else {
+            Err(format!("unexpected string, 'NX' or 'XX' expected"))
+        }
+    }
+}
+
 #[derive(Debug, Clone, StructOpt)]
 pub enum Commands {
     /// set a key with string value
@@ -11,21 +30,16 @@ pub enum Commands {
         /// redis key value
         value: String,
 
-        /// set key expiration in seconds
+        /// set key expiration in seconds, exclusive with px
         #[structopt(short, long)]
         ex: Option<u32>,
 
-        /// set key expiration in milliseconds
+        /// set key expiration in milliseconds, exclusive with ex
         #[structopt(short, long)]
         px: Option<u32>,
 
-        /// operate if key does not exist
-        #[structopt(short, long)]
-        nx: bool,
-
-        /// operate if key does exist
-        #[structopt(short, long)]
-        xx: bool,
+        /// existent flag [NX|XX]
+        x: Option<ExistOP>,
     },
     /// get string value
     Get {
@@ -37,8 +51,9 @@ pub enum Commands {
         /// redis key
         key: String,
     },
+    // TODO 当 position 传入负数时有问题
     /// get list with limit range
-    LRange {
+    Lrange {
         /// redis key
         key: String,
 
@@ -61,8 +76,7 @@ impl Commands {
                 value,
                 ex,
                 px,
-                nx,
-                xx,
+                x,
             } => {
                 let mut count = 3u8;
                 if ex.is_some() {
@@ -71,10 +85,7 @@ impl Commands {
                 if px.is_some() {
                     count += 1
                 }
-                if *nx {
-                    count += 1
-                }
-                if *xx {
+                if x.is_some() {
                     count += 1
                 }
 
@@ -99,13 +110,17 @@ impl Commands {
                     args.push(format!("${}", px.to_string().len()));
                     args.push(px.to_string())
                 }
-                if *nx {
-                    args.push("$2".to_string());
-                    args.push("NX".to_string())
-                }
-                if *xx {
-                    args.push("$2".to_string());
-                    args.push("XX".to_string())
+                if let Some(x) = x {
+                    match x {
+                        ExistOP::NX => {
+                            args.push("$2".to_string());
+                            args.push("NX".to_string())
+                        }
+                        ExistOP::XX => {
+                            args.push("$2".to_string());
+                            args.push("XX".to_string())
+                        }
+                    }
                 }
                 command.put(&args.join("\r\n").to_string().into_bytes()[..]);
                 command.put(&b"\r\n"[..]);
@@ -118,7 +133,7 @@ impl Commands {
             Commands::Incr { key } => command.put(
                 &format!("*2\r\n$4\r\nINCR\r\n${}\r\n{}\r\n", key.len(), key).into_bytes()[..],
             ),
-            Commands::LRange { key, start, stop } => {
+            Commands::Lrange { key, start, stop } => {
                 let args = vec![
                     "*4".to_string(),
                     "$6".to_string(),
